ea4b6d32efcb7ce4513fd171ec9cac19d84f3e30
dreamwkr17 <dreamwkr17@gmail.com>
2013-10-26 04:55:19 +0900

    no message

--- src/com/example/happythonbookproject/MainActivity.java
+++ src/com/example/happythonbookproject/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.happythonbookproject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	// URL 주소 (임시)
+	private String URL = "http://ec2-54-250-200-33.ap-northeast-1.compute.amazonaws.com/cafe_info/?CAFE_NAME=";
+	String keyword;
+	JSONObject jsonobject;
+	JSONArray jsonarray;
+	ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+
+	
+	ListView listview_book;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.d("d","d");
-		Log.d("d","d");
+
+		arraylist = new ArrayList<HashMap<String, String>>();
+		jsonarray = JSONfunctions.getJSONfromURL(URL + keyword);
+		
+		listview_book = (ListView) findViewById(R.id.listview);
+		
+		
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
