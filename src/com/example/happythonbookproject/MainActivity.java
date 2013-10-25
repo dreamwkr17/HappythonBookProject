bb6ef3e32f469dd016523fb0a3359216f7838b4d
dreamwkr17 <dreamwkr17@gmail.com>
2013-10-26 01:06:01 +0900

    commit2

--- src/com/example/happythonbookproject/MainActivity.java
+++ src/com/example/happythonbookproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.happythonbookproject;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d("d","d");
 	}
 
 	@Override
